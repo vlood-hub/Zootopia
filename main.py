58adf9a60f203da4e3e2a89c01c99d1dd51a998a
Vlood <spoke@mac.speedport.ip>
2025-09-24 22:08:24 +0200

    new repository design

--- main.py
+++ main.py
@@ -2,40 +2,58 @@ import json
 
 
 def load_data(file_path):
-  """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+    """ Loads a JSON file """
+    with open(file_path, "r") as handle:
+        return json.load(handle)
 
 
-animals_data = load_data('animals_data.json')
+def read_write_file(file_path, attr, obj=''):
+    """ Read and writes html files """
+    if attr == 'read':
+        with open(file_path, "r", encoding="utf8") as file_in:
+            orig_html_file = file_in.read()
+            return orig_html_file
+    elif attr == 'write':
+        with open(file_path, "w", encoding="utf8") as file_out:
+            file_out.write(obj)
 
-with open("animals_template.html", "r", encoding="utf8") as filein:
-   html_file = filein.read()
 
-output = ''
-for animal_data in animals_data:
+def serialize_animal(animal_data):
+    """ Serializes animal data """
+    output = ''
     output += '<li class="cards__item">'
-    output += f'<div class="card__title"> {animal_data["name"]}<br/></div>'
-    output += '<p class="card__text">'
-    output += f'<strong>Diet:</strong> Diet: {animal_data['characteristics']['diet']}<br/>'
-    output += f'<strong>Location:</strong> {animal_data['locations'][0]}<br/>'
+    output += f'<div class="card__title"> {animal_data["name"]}</div>'
+    output += '<div class="card__text">'
+    output += '<ul style="list-style-type: square">'
+    output += f'''<li style="color: blue"><strong>Scientific name:</strong>
+                    {animal_data['taxonomy']['scientific_name']}</li>'''
+    output += f'<li><strong>Diet:</strong> {animal_data['characteristics']['diet']}</li>'
+    output += f'<li><strong>Location:</strong> {animal_data['locations'][0]}</li>'
     try:
-        output += f'<strong>Type:</strong> {animal_data['characteristics']['type']}<br/>'
-        output += '</p>'
-        output += '</li>'
+        output += f'<li><strong>Type:</strong> {animal_data['characteristics']['type']}</li>'
+        output += f'<li><strong>Group:</strong> {animal_data['characteristics']['group']}</li>'
     except KeyError:
-       print()
-       continue
+        print()
+    output += '</ul>'
+    output += '</div>'
+    output += '</li>'
 
-new_html_file = html_file.replace('__REPLACE_ANIMALS_INFO__', output)
-with open("animals.html", "w", encoding="utf8") as fileout:
-    fileout.write(new_html_file)
+    return output
 
-    """<li class="cards__item">
-  <div class="card__title">Wire Fox Terrier</div>
-  <p class="card__text">
-      <strong>Diet:</strong> Carnivore<br/>
-      <strong>Location:</strong> North-America and Canada<br/>
-      <strong>Type:</strong> mamal<br/>
-  </p>
-</li>"""
+
+def main():
+    """ Works with input data and modifies the loaded html files """
+    animals_data = load_data('animals_data.json')
+
+    orig_html_file = read_write_file('animals_template.html', 'read')
+
+    output = ''
+    for animal_data in animals_data:
+        output += serialize_animal(animal_data)
+
+    new_html_file = orig_html_file.replace('__REPLACE_ANIMALS_INFO__', output)
+    read_write_file('animals.html', 'write', new_html_file)
+
+
+if __name__ == '__main__':
+    main()
