ad64c7ff1f94901d22ac164fb73f57cbc16f2824
Vlood <spoke@MacBook-Air-von-Vlood.local>
2025-09-27 19:59:29 +0200

    animal by user

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import json
+import requests
 
 
-def load_data(file_path):
+def load_data(name):
     """ Loads a JSON file """
-    with open(file_path, "r") as handle:
-        return json.load(handle)
+    api_url = 'https://api.api-ninjas.com/v1/animals?name={}'.format(name)
+    response = requests.get(api_url, headers={'X-Api-Key': 'NA7s7aSou385tD7Cj00X3Q==egXY7wlFAL2Pi1So'})
+    return json.loads(response.text)
 
 
 def read_write_file(file_path, attr, obj=''):
@@ -56,22 +58,30 @@ def ask_skin_type(animals_data):
             print("Please enter a skin type from the list")
 
 
-
 def main():
     """ Works with input data and modifies the loaded html files """
-    animals_data = load_data('animals_data.json')
+    animal = input("Enter an animal you're searching for: ")
+    animals_data = load_data(animal)
 
     orig_html_file = read_write_file('animals_template.html', 'read')
+    write_new_html_file = 'animals.html'
 
-    skin_type_by_user = ask_skin_type(animals_data)
+    if any(animal_data["name"] == animal.capitalize() for animal_data in animals_data):
+        print(animal)
+        skin_type_by_user = ask_skin_type(animals_data)
 
-    output = ''
-    for animal_data in animals_data:
-        if animal_data['characteristics']['skin_type'] == skin_type_by_user.capitalize():
-            output += serialize_animal(animal_data)
+        output = ''
+        for animal_data in animals_data:
+            if animal_data['characteristics']['skin_type'] == skin_type_by_user.capitalize():
+                output += serialize_animal(animal_data)
+
+        print(f"Website was successfully generated to the file {write_new_html_file}")
+
+    else:
+        output = f'<h2>The animal <span style="color: red">{animal} </span> doesn\'t exist.</h2>'
 
     new_html_file = orig_html_file.replace('__REPLACE_ANIMALS_INFO__', output)
-    read_write_file('animals.html', 'write', new_html_file)
+    read_write_file(write_new_html_file, 'write', new_html_file)
 
 
 if __name__ == '__main__':
